f39193333079f6ededa0268aa8b432425b6ed6f2
Artem <temich28012001@gmail.com>
2023-09-24 22:09:02 +0300

    short version class constructor

--- src/Animal.php
+++ src/Animal.php
@@ -2,16 +2,12 @@
 declare(strict_types=1);
 class Animal
 {
-    private string $name;
-    private int $limbs;
 
     public function __construct(
-        string $name,
-        int $limbs
+        private string $name,
+        private int $limbs
     )
     {
-        $this->name = $name;
-        $this->limbs = $limbs;
     }
 }
 
--- src/public/index.php
+++ src/public/index.php
@@ -2,7 +2,8 @@
 declare(strict_types = 1);
 require_once '../Animal.php';
 
-
+$dog = new Animal('dog', 4);
+var_dump($dog);
 
 
 ?>
